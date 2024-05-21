4b2b8e1a7c51a9586a4b75c62b92467b7fb568fd
Maciej Wilewski <m.m.wilewski@gmail.com>
2024-05-22 01:25:35 +0200

    frot login itp

--- bazyy/urls.py
+++ bazyy/urls.py
@@ -44,4 +44,9 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("", hero_page, name="hero_page"),
+    path("main/", main_page, name="main_page"),
+    path("signup/", register_view, name="register"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
